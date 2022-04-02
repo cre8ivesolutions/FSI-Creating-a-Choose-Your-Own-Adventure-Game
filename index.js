33d107152a328c1cba6a77427d675212668af364
cre8ivesolutions <101746947+cre8ivesolutions@users.noreply.github.com>
2022-04-02 12:57:47 -0500

    fourth

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 //Your Code here
+response = window.prompt("Hello!")
+if response === ["hi", "hello", "hi" {
+     window.prompt ("Nice to meet you")
+} else {
+} window.prompt ("I wish you were nicer")
